9c5d23ff92d5a872bca4845d531706417df18ebb
RuanQiaoYun <1846828911@qq.com>
2024-06-11 16:15:52 +0800

    gitHub测试项目HelloWorld——第二次提交

--- gitHubTest_HelloWorld/gitHubTest_HelloWorld/源.cpp
+++ gitHubTest_HelloWorld/gitHubTest_HelloWorld/源.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main() {
 
 	cout << "Hello,World!" << endl;
+	cout << "I feel so sad,and don't have desire to live." << endl;
+	cout << "I'm so useless,and can't do anything well." << endl;
+	cout << "I think I'm a jocker." << endl;
 	system("pause");
 	return 0;
 }
